7a0d85c5eb9c460db6cd8d85089245479bba06e1
Mikołaj Urbański <mikiurb56@gmail.com>
2025-10-25 16:51:12 +0200

    .

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,10 +1,13 @@
 #pragma once
+#include "Resource.hpp"
 
 class ResourceManager
 {
 public:
     ResourceManager() : res(new Resource()) {}
+
     ResourceManager(const ResourceManager& other) : res(new Resource(*other.res)) {}
+
     ResourceManager& operator=(const ResourceManager& other)
     {
         if (this == &other)
@@ -14,8 +17,20 @@ public:
         return *this;
     }
 
+    ResourceManager(ResourceManager&& other) noexcept : res(other.res) { other.res = nullptr; }
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this == &other)
+            return *this;
+        delete res;
+        res       = other.res;
+        other.res = nullptr;
+        return *this;
     }
+
     ~ResourceManager() { delete res; }
+
     double get() const { return res->get(); }
 
 private:
